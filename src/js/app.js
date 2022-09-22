a06557a983664c122a58f6171907d3d0e2c5e9b3
Vad-Dem <vadikdemidenko@i.ua>
2022-09-22 14:29:48 +0300

    finish proj

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,8 @@
 import * as flsFunction from "./function.js";
-
+import WOW from "../../node_modules/wowjs/dist/wow.js";
 document.addEventListener("DOMContentLoaded", function () {
   flsFunction.isWebp();
+  new WOW.WOW({
+    mobile: false,
+  }).init();
 });
